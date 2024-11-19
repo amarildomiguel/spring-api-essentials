c3babf99703bae27a48ebe7f9a6660ba4cff2dde
Amarildo Miguel <amarildomgl@gmail.com>
2024-11-19 11:08:34 +0100

    update properties

--- src/main/java/ucan/edu/sg_acad/SgAcadApplication.java
+++ src/main/java/ucan/edu/sg_acad/SgAcadApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SgAcadApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SgAcadApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SgAcadApplication.class, args);
+    }
 }
